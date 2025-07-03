4a865339bdc1b39ca477d1dc21750ec37773e833
C. M. de Picciotto <d3p1@d3p1.dev>
2025-07-03 01:20:30 +0000

    feat: add lesson number 01 [#1]

--- src/js/card/component/Card.tsx
+++ src/js/card/component/Card.tsx
@@ -2,18 +2,41 @@
  * @description Card
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
-export default function Card() {
+import {useState} from 'react'
+
+export default function Card({
+  fullName,
+  username,
+}: {
+  fullName: string
+  username: string
+}) {
+  const [isFollowing, setIsFollowing] = useState(false)
+
+  const handleOnClick = () => {
+    setIsFollowing(!isFollowing)
+  }
+
   return (
-    <article>
+    <article className="rounded-2xl bg-primary-700 p-4 flex justify-between gap-10 inset-shadow-[0_0_1rem_black] text-xs">
       <section>
-        <img src="https://unavatar.io/midudev" alt="Profile picture" />
+        <img
+          className="rounded-full max-w-[100px]"
+          src={`https://unavatar.io/${username}`}
+          alt="Profile picture"
+        />
       </section>
-      <section>
-        <h2>Name Lastname</h2>
-        <span>@username</span>
+      <section className="flex flex-col justify-center gap-1">
+        <h3>{fullName}</h3>
+        <span className="text-primary-300">{username}</span>
       </section>
-      <aside>
-        <button>Follow</button>
+      <aside className="flex flex-col justify-center w-1/3">
+        <button
+          className="bg-secondary text-primary-900 p-5 rounded-full font-black cursor-pointer"
+          onClick={handleOnClick}
+        >
+          {isFollowing ? 'Unfollow' : 'Follow'}
+        </button>
       </aside>
     </article>
   )
--- src/js/core/component/App.tsx
+++ src/js/core/component/App.tsx
@@ -2,8 +2,64 @@
  * @description App
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
+import {useState} from 'react'
 import Card from '../../card/component/Card.tsx'
 
+const dummyUsers = [
+  {
+    fullName: 'Miguel Ángel Durán',
+    username: 'midudev',
+  },
+  {
+    fullName: 'Cristian Marcelo de Picciotto',
+    username: 'd3p1',
+  },
+  {
+    fullName: 'MR. Doob',
+    username: 'mrdoob',
+  },
+  {
+    fullName: 'Wawa Sensei',
+    username: 'wawasensei',
+  },
+  {
+    fullName: 'Bruno Simon',
+    username: 'bruno_simon',
+  },
+  {
+    fullName: 'Simon',
+    username: 'iced_coffee_dev',
+  },
+  {
+    fullName: 'Jhey',
+    username: 'jh3yy',
+  },
+  {
+    fullName: 'Sarlloc',
+    username: 'sarlloc',
+  },
+]
+
 export default function App() {
-  return <Card />
+  const [users, setUsers] = useState(dummyUsers.slice(0, 2))
+
+  const handleOnClick = () => {
+    const index = Math.floor(Math.random() * (dummyUsers.length - 2))
+    setUsers(dummyUsers.slice(index, index + 2))
+  }
+
+  return (
+    <section className="flex flex-col gap-4">
+      {users.map(({fullName, username}) => (
+        <Card fullName={fullName} username={username} key={username} />
+      ))}
+
+      <button
+        className="rounded-full bg-secondary text-primary-900 font-black p-5 cursor-pointer"
+        onClick={handleOnClick}
+      >
+        Refresh
+      </button>
+    </section>
+  )
 }
