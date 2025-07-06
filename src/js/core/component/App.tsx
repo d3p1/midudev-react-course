421a1639e673b11284d6bef8f7febbe845058478
C. M. de Picciotto <d3p1@d3p1.dev>
2025-07-06 23:05:38 +0000

    feat: add keydown handler to move between lessons [#1]

--- src/js/core/component/App.tsx
+++ src/js/core/component/App.tsx
@@ -2,52 +2,43 @@
  * @description App
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
-import {useState, createElement} from 'react'
-import {lessons} from '../utils/lessons.ts'
+import {createElement} from 'react'
+import useLesson from '../hook/useLesson.ts'
 
 export default function App() {
-  const [index, setIndex] = useState(0)
-
-  const handleClick = (step: number) => {
-    let processedIndex = (index + step) % lessons.length
-    if (processedIndex < 0) {
-      processedIndex = lessons.length - 1
-    }
-
-    setIndex(processedIndex)
-  }
+  const {lesson, refreshLesson} = useLesson()
 
   return (
     <main className="w-full h-full grid place-items-center">
       <h1 className="absolute top-10 uppercase font-black">
         <a
-          href={lessons[index].link}
+          href={lesson.link}
           target="_blank"
           className="underline cursor-pointer"
         >
-          <span className="mr-2">[{`Lesson ${lessons[index].number}`}]</span>
-          <span>{lessons[index].title}</span>
+          <span className="mr-2">[{`Lesson ${lesson.number}`}]</span>
+          <span>{lesson.title}</span>
         </a>
       </h1>
 
-      {lessons[index].additionalNote && (
+      {lesson.additionalNote && (
         <div className="absolute top-20">
-          {createElement(lessons[index].additionalNote)}
+          {createElement(lesson.additionalNote)}
         </div>
       )}
 
       <div className="absolute bottom-10 w-full flex justify-between p-10">
         <button
-          className="relative z-10 h-0 w-0 border-y-16 border-y-transparent border-r-16 border-r-primary-200 cursor-pointer"
-          onClick={() => handleClick(-1)}
+          className="relative z-10 h-0 w-0 border-y-16 border-y-transparent border-r-16 border-r-primary-200 cursor-pointer hover:border-r-primary-600"
+          onClick={() => refreshLesson(-1)}
         ></button>
         <button
-          className="relative z-10 h-0 w-0 border-y-16 border-y-transparent border-l-16 border-l-primary-200 cursor-pointer"
-          onClick={() => handleClick(+1)}
+          className="relative z-10 h-0 w-0 border-y-16 border-y-transparent border-l-16 border-l-primary-200 cursor-pointer hover:border-l-primary-600"
+          onClick={() => refreshLesson(+1)}
         ></button>
       </div>
 
-      {createElement(lessons[index].component)}
+      {createElement(lesson.component)}
     </main>
   )
 }
---
+++ src/js/core/hook/useLesson.ts
@@ -0,0 +1,43 @@
+/**
+ * @description Hook to manage lessons
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+import {useState, useEffect} from 'react'
+import type {Lesson} from '../../types'
+import {lessons} from '../utils/lessons.ts'
+
+let index = 0
+export default function useLesson() {
+  const [lesson, setLesson] = useState<Lesson>(lessons[index])
+
+  const refreshLesson = (step: number) => {
+    index = (index + step) % lessons.length
+    if (index < 0) {
+      index = lessons.length - 1
+    }
+
+    setLesson(lessons[index])
+  }
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      switch (e.key) {
+        case 'ArrowRight':
+          refreshLesson(+1)
+          break
+
+        case 'ArrowLeft':
+          refreshLesson(-1)
+          break
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown)
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [])
+
+  return {lesson, refreshLesson}
+}
