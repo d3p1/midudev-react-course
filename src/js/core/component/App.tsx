3eec08344aab2df3d3d596a5adf286212fdaa1b6
C. M. de Picciotto <d3p1@d3p1.dev>
2025-07-08 16:55:13 +0000

    fix: adjust core styles [#1]

--- src/js/core/component/App.tsx
+++ src/js/core/component/App.tsx
@@ -9,36 +9,38 @@ export default function App() {
   const {lesson, refreshLesson} = useLesson()
 
   return (
-    <main className="w-full h-full grid place-items-center">
-      <h1 className="absolute top-10 uppercase font-black">
-        <a
-          href={lesson.link}
-          target="_blank"
-          className="underline cursor-pointer"
-        >
-          <span className="mr-2">[{`Lesson ${lesson.number}`}]</span>
-          <span>{lesson.title}</span>
-        </a>
-      </h1>
+    <div className="w-screen h-screen grid grid-rows-[1fr_5fr_1fr] place-items-center">
+      <header className="w-full flex flex-col justify-center items-center gap-4">
+        <h1 className="uppercase font-black">
+          <a
+            href={lesson.link}
+            target="_blank"
+            className="underline cursor-pointer"
+          >
+            <span className="mr-2">[{`Lesson ${lesson.number}`}]</span>
+            <span>{lesson.title}</span>
+          </a>
+        </h1>
 
-      {lesson.additionalNote && (
-        <div className="absolute top-20">
-          {createElement(lesson.additionalNote)}
-        </div>
-      )}
+        {lesson.additionalNote && (
+          <div>{createElement(lesson.additionalNote)}</div>
+        )}
+      </header>
 
-      <div className="absolute bottom-10 w-full flex justify-between p-10">
+      <main className="w-full max-h-10/11 flex justify-center items-center overflow-y-auto">
+        {createElement(lesson.component)}
+      </main>
+
+      <div className="w-full flex justify-between p-10">
         <button
-          className="relative z-10 h-0 w-0 border-y-16 border-y-transparent border-r-16 border-r-primary-200 cursor-pointer hover:border-r-primary-600"
+          className="h-0 w-0 border-y-16 border-y-transparent border-r-16 border-r-primary-200 cursor-pointer hover:border-r-primary-600"
           onClick={() => refreshLesson(-1)}
         ></button>
         <button
-          className="relative z-10 h-0 w-0 border-y-16 border-y-transparent border-l-16 border-l-primary-200 cursor-pointer hover:border-l-primary-600"
+          className="h-0 w-0 border-y-16 border-y-transparent border-l-16 border-l-primary-200 cursor-pointer hover:border-l-primary-600"
           onClick={() => refreshLesson(+1)}
         ></button>
       </div>
-
-      {createElement(lesson.component)}
-    </main>
+    </div>
   )
 }
