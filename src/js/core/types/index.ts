ce5492262419fd4b732361ffbf83623b0736d0a3
C. M. de Picciotto <d3p1@d3p1.dev>
2025-07-15 23:05:03 +0000

    feat: add lazy loading feature to lessons [#1]

--- src/js/core/types/index.ts
+++ src/js/core/types/index.ts
@@ -2,14 +2,14 @@
  * @description Types
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
-import type {JSX} from 'react'
+import type {JSX, LazyExoticComponent} from 'react'
 
 export interface Lesson {
   number: string
   title: string
   link: string
-  additionalNote?: () => JSX.Element
-  component: () => JSX.Element
+  additionalNote?: (() => JSX.Element) | LazyExoticComponent<() => JSX.Element>
+  component: (() => JSX.Element) | LazyExoticComponent<() => JSX.Element>
 }
 
 export type Lessons = Array<Lesson>
--- src/js/core/utils/lessons.ts
+++ src/js/core/utils/lessons.ts
@@ -2,14 +2,7 @@
  * @description Lessons
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
-import Lesson01 from '../../lesson01/component/App.tsx'
-import Lesson02 from '../../lesson02/component/App.tsx'
-import Lesson03 from '../../lesson03/component/App.tsx'
-import Lesson04 from '../../lesson04/component/App.tsx'
-import Lesson05 from '../../lesson05/component/App.tsx'
-import Lesson06 from '../../lesson06/component/App.tsx'
-import Lesson07 from '../../lesson07/component/App.tsx'
-import AdditionalNote from '../../lesson04/component/app/AdditionalNote.tsx'
+import {lazy} from 'react'
 import type {Lessons} from '../types'
 
 export const lessons: Lessons = [
@@ -17,43 +10,45 @@ export const lessons: Lessons = [
     number: '01',
     title: 'Twitter Card',
     link: 'https://cursoreact.dev/01-react-desde-cero',
-    component: Lesson01,
+    component: lazy(() => import('../../lesson01/component/App.tsx')),
   },
   {
     number: '02',
     title: 'Tic-Tac-Toe',
     link: 'https://cursoreact.dev/02-use-state-use-effect',
-    component: Lesson02,
+    component: lazy(() => import('../../lesson02/component/App.tsx')),
   },
   {
     number: '03',
     title: 'Mouse Follower',
     link: 'https://youtu.be/qkzcjwnueLA?t=4040',
-    component: Lesson03,
+    component: lazy(() => import('../../lesson03/component/App.tsx')),
   },
   {
     number: '04',
     title: 'Cat App',
     link: 'https://cursoreact.dev/03-react-prueba-tecnica',
-    additionalNote: AdditionalNote,
-    component: Lesson04,
+    additionalNote: lazy(
+      () => import('../../lesson04/component/app/AdditionalNote.tsx'),
+    ),
+    component: lazy(() => import('../../lesson04/component/App.tsx')),
   },
   {
     number: '05',
     title: 'Movie Search',
     link: 'https://cursoreact.dev/05-use-callback-use-memo-use-ref',
-    component: Lesson05,
+    component: lazy(() => import('../../lesson05/component/App.tsx')),
   },
   {
     number: '06',
     title: 'Shopping Cart',
     link: 'https://cursoreact.dev/06-use-reducer-use-context',
-    component: Lesson06,
+    component: lazy(() => import('../../lesson06/component/App.tsx')),
   },
   {
     number: '07',
     title: 'React Router From Scratch',
     link: 'https://cursoreact.dev/07-midu-router',
-    component: Lesson07,
+    component: lazy(() => import('../../lesson07/component/App.tsx')),
   },
 ]
