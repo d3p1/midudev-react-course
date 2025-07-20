cc36de1a9cec3f8e8aa29703c7ffcbef377b0b2d
C. M. de Picciotto <d3p1@d3p1.dev>
2025-07-20 03:30:10 +0000

    feat: init styles for lesson number 09 [#1]

--- src/js/lesson09/component/App.tsx
+++ src/js/lesson09/component/App.tsx
@@ -2,6 +2,40 @@
  * @description App
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
+import {InterchangeIcon} from './app/icon/InterchangeIcon.tsx'
+
 export default function App() {
-  return <h3>Lesson 09</h3>
+  return (
+    <div className="grid grid-rows-[1fr_4fr] place-items-stretch gap-5 w-1/2">
+      <div className="grid grid-cols-3 place-items-center p-8 bg-primary-800 inset-shadow-[black_0_0_2rem] rounded-2xl">
+        <select className="bg-transparent text-secondary p-2 rounded-lg">
+          <option className="bg-primary-800">Esp</option>
+          <option className="bg-primary-800">Eng</option>
+          <option className="bg-primary-800">Dutch</option>
+        </select>
+        <button className="cursor-pointer">
+          <InterchangeIcon />
+        </button>
+        <select className="bg-transparent text-secondary p-2`` rounded-lg">
+          <option className="bg-primary-800">Esp</option>
+          <option className="bg-primary-800">Eng</option>
+          <option className="bg-primary-800">Dutch</option>
+        </select>
+      </div>
+      <div className="grid grid-cols-2 place-items-stretch p-8 gap-4 bg-primary-800 inset-shadow-[black_0_0_2rem] rounded-2xl italic">
+        <textarea
+          name="from-text"
+          className="p-8 border-primary-700 border-solid border-2 rounded-2xl"
+        >
+          From text...
+        </textarea>
+        <textarea
+          name="to-text"
+          className="p-8 border-primary-700 border-solid border-2 rounded-2xl"
+        >
+          To text...
+        </textarea>
+      </div>
+    </div>
+  )
 }
---
+++ src/js/lesson09/component/app/icon/InterchangeIcon.tsx
@@ -0,0 +1,18 @@
+/**
+ * @description Interchange icon
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+export const InterchangeIcon = () => {
+  return (
+    <svg
+      focusable="false"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="white"
+      height="30"
+      width="30"
+    >
+      <path d="M6.99 11L3 15l3.99 4v-3H14v-2H6.99v-3zM21 9l-3.99-4v3H10v2h7.01v3L21 9z"></path>
+    </svg>
+  )
+}
