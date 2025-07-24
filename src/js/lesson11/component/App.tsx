7f499a03f4137a72eccd4a3797bab407d248fff2
C. M. de Picciotto <d3p1@d3p1.dev>
2025-07-24 23:15:01 +0000

    fix: use reference to restore original users in lesson number 11 [#1]

--- src/js/lesson11/component/App.tsx
+++ src/js/lesson11/component/App.tsx
@@ -2,21 +2,23 @@
  * @description App
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
-import {useEffect, useState} from 'react'
+import {useEffect, useRef, useState} from 'react'
 import type {User, UserId} from '../types'
 import {UserManager} from '../utils/user-manager.ts'
 import {UserTable} from './app/UserTable.tsx'
 
-const userManager = new UserManager()
-
 export default function App() {
+  const originalUsers = useRef<User[]>([])
   const [users, setUsers] = useState<User[]>([])
   const [error, setError] = useState<string | null>(null)
 
   useEffect(() => {
+    const userManager = new UserManager()
+
     userManager
       .loadUsers()
       .then(() => {
+        originalUsers.current = userManager.users
         setUsers(userManager.users)
       })
       .catch((e) => {
@@ -31,7 +33,7 @@ export default function App() {
   }
 
   const handleRestart = () => {
-    setUsers(userManager.users)
+    setUsers(originalUsers.current)
   }
 
   if (error) {
