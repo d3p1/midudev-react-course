86c76dc234ac3afabe3cad7ba8a0108344f71def
C. M. de Picciotto <d3p1@d3p1.dev>
2025-07-28 23:10:15 +0000

    feat: init question manager for lesson number 13 [#1]

---
+++ src/js/lesson13/service/question-manager.ts
@@ -0,0 +1,33 @@
+/**
+ * @description Question manager
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+import type {Question} from '../types'
+
+const QUESTION_API_ENDPOINT =
+  'https://raw.githubusercontent.com/midudev/aprendiendo-react/refs/heads/master/projects/13-javascript-quiz-con-zustand/public/data.json'
+
+export class QuestionManager {
+  /**
+   * Load questions
+   *
+   * @param   {number} limit
+   * @returns {Promise<Question[]>}
+   * @throws  {Error}
+   */
+  static async loadQuestions(limit: number = 10): Promise<Question[]> {
+    const result = await fetch(QUESTION_API_ENDPOINT)
+
+    if (!result.ok) {
+      throw new Error('Failed to fetch questions.')
+    }
+
+    const data = (await result.json()) as Question[]
+
+    if (!data.length) {
+      throw new Error('Failed to load questions.')
+    }
+
+    return data.toSorted(() => (Math.random() > 0.5 ? -1 : 1)).slice(0, limit)
+  }
+}
