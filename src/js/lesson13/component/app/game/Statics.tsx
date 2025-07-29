0a7a5ca033f9e898d72922fe3bc9a0a16d97971f
C. M. de Picciotto <d3p1@d3p1.dev>
2025-07-29 00:10:13 +0000

    feat: add statics to lesson number 13 [#1]

--- src/js/lesson13/component/app/game/Statics.tsx
+++ src/js/lesson13/component/app/game/Statics.tsx
@@ -2,17 +2,32 @@
  * @description Game statics
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
+import {useQuestionStore} from '../../../store/question.ts'
+
 export const Statics = () => {
+  const questions = useQuestionStore((state) => state.questions)
+  const correct = questions.filter(
+    (question) => question.correctAnswer === question.userSelectedAnswer,
+  ).length
+  const incorrect = questions.filter(
+    (question) =>
+      question.userSelectedAnswer !== undefined &&
+      question.correctAnswer !== question.userSelectedAnswer,
+  ).length
+  const pending = questions.filter(
+    (question) => question.userSelectedAnswer === undefined,
+  ).length
+
   return (
     <div className="flex justify-center items-center gap-6 p-4 bg-primary-800 rounded-2xl text-xs">
       <p>
-        <span className="font-black">Correct:</span> <span>10</span>
+        <span className="font-black">Correct:</span> <span>{correct}</span>
       </p>
       <p>
-        <span className="font-black">Incorrect:</span> <span>2</span>
+        <span className="font-black">Incorrect:</span> <span>{incorrect}</span>
       </p>
       <p>
-        <span className="font-black">Pending:</span> <span>3</span>
+        <span className="font-black">Pending:</span> <span>{pending}</span>
       </p>
     </div>
   )
