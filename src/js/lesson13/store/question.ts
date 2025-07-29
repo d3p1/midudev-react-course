ac6830ee64a2c96744374b343f8949ee4e441696
C. M. de Picciotto <d3p1@d3p1.dev>
2025-07-29 00:28:29 +0000

    feat: add redux devtools feature to lesson number 13 [#1]

--- src/js/lesson13/store/question.ts
+++ src/js/lesson13/store/question.ts
@@ -3,7 +3,7 @@
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
 import {create} from 'zustand/react'
-import {persist} from 'zustand/middleware'
+import {persist, devtools} from 'zustand/middleware'
 import type {Question} from '../types'
 import {QuestionManager} from '../service/question-manager.ts'
 
@@ -18,53 +18,75 @@ interface State {
 }
 
 export const useQuestionStore = create<State>()(
-  persist(
-    (set, get) => ({
-      questions: [],
-      currentQuestion: 0,
-      loadQuestions: async () => {
-        const questions = await QuestionManager.loadQuestions()
-        set({
-          questions,
-        })
-      },
-      answer: (questionId, answerIndex) => {
-        const {questions} = get()
-        const newQuestions = structuredClone(questions)
-        const questionIndex = newQuestions.findIndex(
-          (question) => question.id === questionId,
-        )
-        newQuestions[questionIndex].userSelectedAnswer = answerIndex
+  devtools(
+    persist(
+      (set, get) => ({
+        questions: [],
+        currentQuestion: 0,
+        loadQuestions: async () => {
+          const questions = await QuestionManager.loadQuestions()
+          set(
+            {
+              questions,
+            },
+            false,
+            'LOAD_QUESTIONS',
+          )
+        },
+        answer: (questionId, answerIndex) => {
+          const {questions} = get()
+          const newQuestions = structuredClone(questions)
+          const questionIndex = newQuestions.findIndex(
+            (question) => question.id === questionId,
+          )
+          newQuestions[questionIndex].userSelectedAnswer = answerIndex
 
-        set({
-          questions: newQuestions,
-        })
+          set(
+            {
+              questions: newQuestions,
+            },
+            false,
+            'ANSWER',
+          )
+        },
+        goNextQuestion: () => {
+          const {questions, currentQuestion} = get()
+          if (currentQuestion < questions.length - 1) {
+            set(
+              {
+                currentQuestion: currentQuestion + 1,
+              },
+              false,
+              'GO_NEXT_QUESTION',
+            )
+          }
+        },
+        goPreviousQuestion: () => {
+          const {currentQuestion} = get()
+          if (currentQuestion > 0) {
+            set(
+              {
+                currentQuestion: currentQuestion - 1,
+              },
+              false,
+              'GO_PREV_QUESTION',
+            )
+          }
+        },
+        restart: () => {
+          set(
+            {
+              questions: [],
+              currentQuestion: 0,
+            },
+            false,
+            'RESTART',
+          )
+        },
+      }),
+      {
+        name: 'questions',
       },
-      goNextQuestion: () => {
-        const {questions, currentQuestion} = get()
-        if (currentQuestion < questions.length - 1) {
-          set({
-            currentQuestion: currentQuestion + 1,
-          })
-        }
-      },
-      goPreviousQuestion: () => {
-        const {currentQuestion} = get()
-        if (currentQuestion > 0) {
-          set({
-            currentQuestion: currentQuestion - 1,
-          })
-        }
-      },
-      restart: () => {
-        set({
-          questions: [],
-          currentQuestion: 0,
-        })
-      },
-    }),
-    {
-      name: 'questions',
-    },
+    ),
   ),
 )
