59f0fc647db12bd1990f8445a2e4a857636f7c80
C. M. de Picciotto <d3p1@d3p1.dev>
2025-07-29 00:13:35 +0000

    feat: add local storage persist feature to lesson number 13 [#1]

--- src/js/lesson13/store/question.ts
+++ src/js/lesson13/store/question.ts
@@ -3,6 +3,7 @@
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
 import {create} from 'zustand/react'
+import {persist} from 'zustand/middleware'
 import type {Question} from '../types'
 import {QuestionManager} from '../service/question-manager.ts'
 
@@ -16,47 +17,54 @@ interface State {
   restart: () => void
 }
 
-export const useQuestionStore = create<State>((set, get) => ({
-  questions: [],
-  currentQuestion: 0,
-  loadQuestions: async () => {
-    const questions = await QuestionManager.loadQuestions()
-    set({
-      questions,
-    })
-  },
-  answer: (questionId, answerIndex) => {
-    const {questions} = get()
-    const newQuestions = structuredClone(questions)
-    const questionIndex = newQuestions.findIndex(
-      (question) => question.id === questionId,
-    )
-    newQuestions[questionIndex].userSelectedAnswer = answerIndex
-
-    set({
-      questions: newQuestions,
-    })
-  },
-  goNextQuestion: () => {
-    const {questions, currentQuestion} = get()
-    if (currentQuestion < questions.length - 1) {
-      set({
-        currentQuestion: currentQuestion + 1,
-      })
-    }
-  },
-  goPreviousQuestion: () => {
-    const {currentQuestion} = get()
-    if (currentQuestion > 0) {
-      set({
-        currentQuestion: currentQuestion - 1,
-      })
-    }
-  },
-  restart: () => {
-    set({
+export const useQuestionStore = create<State>()(
+  persist(
+    (set, get) => ({
       questions: [],
       currentQuestion: 0,
-    })
-  },
-}))
+      loadQuestions: async () => {
+        const questions = await QuestionManager.loadQuestions()
+        set({
+          questions,
+        })
+      },
+      answer: (questionId, answerIndex) => {
+        const {questions} = get()
+        const newQuestions = structuredClone(questions)
+        const questionIndex = newQuestions.findIndex(
+          (question) => question.id === questionId,
+        )
+        newQuestions[questionIndex].userSelectedAnswer = answerIndex
+
+        set({
+          questions: newQuestions,
+        })
+      },
+      goNextQuestion: () => {
+        const {questions, currentQuestion} = get()
+        if (currentQuestion < questions.length - 1) {
+          set({
+            currentQuestion: currentQuestion + 1,
+          })
+        }
+      },
+      goPreviousQuestion: () => {
+        const {currentQuestion} = get()
+        if (currentQuestion > 0) {
+          set({
+            currentQuestion: currentQuestion - 1,
+          })
+        }
+      },
+      restart: () => {
+        set({
+          questions: [],
+          currentQuestion: 0,
+        })
+      },
+    }),
+    {
+      name: 'questions',
+    },
+  ),
+)
